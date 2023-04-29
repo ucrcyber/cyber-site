da1984ec81ef09dbca13142478d5915c26714686
NateM135 <melwaninate@gmail.com>
2023-04-28 20:45:08 -0700

    updated calendar for current year

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -1,56 +1,61 @@
 <template>
-<div>
-  <sui-segment vertical class="stripe">
-    <div class="ui middle aligned stackable grid container">
-      <div class="row">
-        <div class="eight wide left column">
-          <h3 class="ui header">Meetings: Spring 2022</h3>
-          <!-- <p> General meetings cover announcements, defensive/offensive demos, tech-talks, and guest speakers. We also host weekly practices for Defensive and Offensive competitions! Anyone is free to join. Email <b> cyberucr@gmail.com </b> for more info. </p>  -->
-          <p> This Spring we're hosting biweekly General Meetings alongside defensive and offensive security workshop meetings. Download our Google calendar to yours! </p> 
-          <p> Got questions? Email <b> ucrcyber@gmail.com </b> </p>
-          <p>General Meetings: Odd Weeks Wednesday @2pm. </p>
-          <p>CCDC Meetings: TBA on Slack and Discord</p>
-          <p>CPTC Meetings: TBA on Slack and Discord</p>	
-          <p>CTF: Check Calendar and Slack/Discord</p>	
-        </div>
+  <div>
+    <sui-segment vertical class="stripe">
+      <div class="ui middle aligned stackable grid container">
+        <div class="row">
+          <div class="eight wide left column">
+            <h3 class="ui header">Meetings: Spring 2023</h3>
+            <!-- <p> General meetings cover announcements, defensive/offensive demos, tech-talks, and guest speakers. We also host weekly practices for Defensive and Offensive competitions! Anyone is free to join. Email <b> cyberucr@gmail.com </b> for more info. </p>  -->
+            <p>
+              This Spring we're hosting biweekly General Meetings alongside
+              defensive and offensive security workshop meetings. Download our
+              Google calendar to yours!
+            </p>
+            <p>Got questions? Email <b> ucrcyber@gmail.com </b></p>
+            <p>General Meetings: Even Weeks Thursday @3:30pm.</p>
+            <p>CCDC Meetings: Check Calendar and Discord</p>
+            <p>CPTC Meetings: Check Calendar and Discord</p>
+            <p>CTF: Check Calendar and Discord</p>
+          </div>
 
-        <div class="eight wide right floated column">
-          <iframe src="https://calendar.google.com/calendar/embed?src=cyberucr%40gmail.com&ctz=America%2FLos_Angeles" style="border: 0" width="600" height="400" frameborder="0" scrolling="no"></iframe>
+          <div class="eight wide right floated column">
+            <iframe
+              src="https://calendar.google.com/calendar/embed?src=cyberucr%40gmail.com&ctz=America%2FLos_Angeles"
+              style="border: 0"
+              width="600"
+              height="400"
+              frameborder="0"
+              scrolling="no"
+            ></iframe>
+          </div>
         </div>
       </div>
-    </div>
-  </sui-segment>
+    </sui-segment>
 
-  <sui-segment vertical class="stripe">
-    <div class="ui text container">
-      
-    </div>
-  </sui-segment>
-
-</div>
-  
+    <sui-segment vertical class="stripe">
+      <div class="ui text container"></div>
+    </sui-segment>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      title: 'Calendar',
-      description: 'Cyber@UCR\'s Upcoming Events'
-    }
+      title: "Calendar",
+      description: "Cyber@UCR's Upcoming Events",
+    };
   },
-   head () {
+  head() {
     return {
-      title: this.title
-    }
+      title: this.title,
+    };
   },
-  mounted () {
-    this.$store.commit('SET_PAGE_TITLE', this.title)
-    this.$store.commit('SET_PAGE_DESCRIPTION', this.description)
-  }
+  mounted() {
+    this.$store.commit("SET_PAGE_TITLE", this.title);
+    this.$store.commit("SET_PAGE_DESCRIPTION", this.description);
+  },
 };
 </script>
 
-<style>
-  
-</style>
+<style></style>
