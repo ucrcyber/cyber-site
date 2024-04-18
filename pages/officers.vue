09c74e7b60e2e10500482d9cb952478070b16a5a
Sped32DJ <dannycool10236@gmail.com>
2024-04-18 10:55:18 -0700

    Adding new officers and deleting old; TODO, add photos + bios

--- pages/officers.vue
+++ pages/officers.vue
@@ -31,42 +31,28 @@ export default {
   data() {
     return {
       title: "Officers",
-      description: "Cyber@UCR 2023-2024 Board",
+      description: "Cyber@UCR 2024-2025 Board",
       officers: [
-        {
-          name: "Nathan Melwani",
-          image: require("~/assets/officers/nathan.jpg"),
-          position: "President",
-          email: "nmelw002@ucr.edu",
-          description: `Nathan is the President of Cyber@UCR. He serves as the captain of the CPTC Team, Networks/Linux Admin of the CCDC team, captain of the CTF team, and captain of teams that are formed to play in smaller competitions. Majoring in CS, he primarily focuses on web application security and network security.`,
-        },
         {
           name: "Justin Lee",
           image: require("~/assets/officers/justin.jpeg"),
-          position: "Vice President",
+          position: "President",
           email: "jlee1442@ucr.edu",
           description: `Justin is the Vice President for Cyber@UCR. His duty is to ensure that all club-related activities are ethical and conducted without malicious intent, as well as to manage any liabilities that may result from unethical activities. He will help Cyber better organize and maintain its vital documents. He is primarily focused on Unix Systems and Networks.`,
         },
         {
-          name: "Adil Mohiuddin",
-          image: require("~/assets/officers/adil.jpg"),
-          position: "Ethics Director",
-          email: "amohi010@ucr.edu",
-          description: `Adil is the Ethics Director for Cyber@UCR. His role is to assist the president in their role. He also serves as one of the Linux admins on our CCDC team. As a second-year majoring in computer engineering, he primarily enjoys creating secure open source software.  Correspondingly, he hopes to make something useful someday. He has a slight obsession with the arch distribution.`,
-        },
-        {
-          name: "Melanie Gomez",
-          image: require("~/assets/officers/melanie.jpg"),
-          position: "Treasurer",
-          email: "mgome175@ucr.edu",
-          description: `Melanie is the treasurer for Cyber@UCR. Her duties involve activating a club account, creating and proposing the club budget to ASUCR, creating and proposing a match fund budget, ordering club materials, maintaining a balance sheet of club transactions, and ordering food for club meetings. She hopes to increase fundings and efficiently manage all the finance of the club to ensure none of the activities or events are limited by any matters regarding finance, giving the best experience for all of Cyber’s members. Majoring in Computer Science, she is very interested in software engineering and cybersecurity. She hopes to further expand her knowledge in such fields in the upcoming year.`,
-        },
-        {
           name: "Joshua Hsu",
           image: require("~/assets/officers/joshua.png"),
-          position: "Secretary",
+          position: "Vice President",
           email: "jhsu094@ucr.edu",
           description: `Joshua serves as the Secretary for Cyber@UCR. His duty is helping maintain the club's official standing as well as organizing the club's official documents. Majoring in Computer Science, Joshua is primarily interested in game development, software engineering, cybersecurity, and the intertwining of the different fields.`,
+        },
+        {
+          name: "Kareem LastName",
+          image: require("~/assets/officers/kareem.jpg"),
+          position: "secretary",
+          email: "netID@ucr.edu",
+          description: `Kareem is the secretary for Cyber@UCR BLANK_SPACE`,
         },
         {
           name: "Anphu Dung",
@@ -78,9 +64,16 @@ export default {
         {
           name: "Thomas Li",
           image: require("~/assets/officers/thomas.jpg"),
-          position: "General Project Lead",
+          position: "Ethics Director",
           email: "tli186@ucr.edu",
           description: `The General Project Lead for Cyber@UCR is responsible for overseeing the overall direction and management of the club's projects and initiatives related to cybersecurity at the University of California, Riverside. They collaborate with other club leaders and members to ensure the successful execution of activities, events, and campaigns that promote cybersecurity awareness and education on campus.`,
+        },
+        {
+          name: "Mark Ghebrial",
+          image: require("~/assets/officers/mark.jpg"),
+          position: "Treasurer",
+          email: "mgheb003@ucr.edu",
+          description: `Mark is the Treasurer for Cyber@UCR BLANK_SPACE`,
         },
         {
           name: "Chloe Au",
