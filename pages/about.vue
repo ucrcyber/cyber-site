5637c15b031162e98117b731c5c30ebd8ff0fd9e
Sped32DJ <dannycool10236@gmail.com>
2024-04-26 23:43:16 -0700

    Only ccdc24 Qualifiers so far

--- pages/about.vue
+++ pages/about.vue
@@ -22,27 +22,44 @@
         <li><p>Security Awareness Training</p></li>
         <li><p>Locksport</p></li>
         <li><p>Ethical Disclosure Practices</p></li>
-      </ul> 
+      </ul>
 
       <h2 class="ui header">Some competitions we participate in</h2>
       <ul>
         <li><p>Collegiate Cyber Defense Competition</p></li>
         <li><p>Collegiate Penetration Testing Competition</p></li>
-      </ul> 
+      </ul>
 
       <h2 class="ui header">Check out our practice groups</h2>
       <ul>
         <li><a href="https://www.hackthebox.eu/home/universities/profile/350">HackTheBox</a></li>
         <li><a href="https://ctftime.org/team/109680">CTFtime</a></li>
-      </ul> 
+      </ul>
     </div>
   </sui-segment>
 
   <sui-segment vertical class="stripe">
     <div class="ui middle aligned stackable grid container">
       <div class="row">
-        <div class="center aligned column">      
+        <div class="center aligned column">
           <a class="ui huge button" href="https://discord.com/invite/vESWWrm">Interested in Competing?</a>
+        </div>
+      </div>
+
+       <div class="row">
+        <div class="eight wide column">
+          <h3 class="ui header">
+            2024 CCDC Qualifiers Team
+          </h3>
+          <p>
+            Cyber@UCR places 6th and makes it into WRCCDC Regionals!
+          </p>
+          <p>
+            Captain: Nathan Melwani, Team: Kareem Shehada, Mark Ghebrial, AnPhu Dung, Joshua Hsu, Justin Lee, Thomas Li, Gryphon Kumfert
+          </p>
+        </div>
+        <div class="six wide right floated column">
+          <img class="ui large bordered rounded image" src="~/assets/competitions/ccdcTeam2024.jpg" />
         </div>
       </div>
 
@@ -52,10 +69,10 @@
             2022 Information Security Talent Search (ISTS)
           </h3>
           <p>
-            Cyber@UCR places 4th at RIT's Information Security Talent Search! 
-          </p> 
+            Cyber@UCR places 4th at RIT's Information Security Talent Search!
+          </p>
           <p>
-            Captain: Nathan Melwani, Team: Jason Lin, Anthony Hallak, Kobe Braga, Jiera Sirivatanarat  
+            Captain: Nathan Melwani, Team: Jason Lin, Anthony Hallak, Kobe Braga, Jiera Sirivatanarat
           </p>
         </div>
         <div class="six wide right floated column">
@@ -69,10 +86,10 @@
             2022 Regionals CCDC Team
           </h3>
           <p>
-            Cyber@UCR places 4th at Western Regionals! 
-          </p>  
+            Cyber@UCR places 4th at Western Regionals!
+          </p>
           <p>
-            Captain: Anthony Hallak, Co-captain/Networks/Linux: Nathan Melwani, Windows: Jiera Sirivatanarat, Web: Kobe Braga, Injects: Kanin Liang, Linux: Adil Mohiuddin, Linux/Networks: Estela Ramirez 
+            Captain: Anthony Hallak, Co-captain/Networks/Linux: Nathan Melwani, Windows: Jiera Sirivatanarat, Web: Kobe Braga, Injects: Kanin Liang, Linux: Adil Mohiuddin, Linux/Networks: Estela Ramirez
           </p>
         </div>
         <div class="six wide right floated column">
@@ -86,13 +103,13 @@
             2021   Information Security Talent Search (ISTS)
           </h3>
           <p>
-            Cyber@UCR places 2nd at RIT's Information Security Talent Search! 
-          </p> 
+            Cyber@UCR places 2nd at RIT's Information Security Talent Search!
+          </p>
           <p>
             Rankings for ISTS on <a href="https://twitter.com/ritsecclub/status/1370431327493820419">Twitter</a>. <br>
           </p>
           <p>
-            Captain: Nathan Melwani, Team: Jason Lin, Anthony Hallak, Kobe Braga, Ethan Roos 
+            Captain: Nathan Melwani, Team: Jason Lin, Anthony Hallak, Kobe Braga, Ethan Roos
           </p>
         </div>
         <div class="six wide right floated column">
@@ -113,7 +130,7 @@
             Recognition from UC Riverside on <a href="https://twitter.com/UCRiverside/status/1346860974301564929">Twitter</a>.
           </p>
           <p>
-            Captain: Luis Garcia, Co-captain: Jason Lin, Members: Jiera Sirivatanarat, Anthony Hallak, Nathan Melwani, Elise Lin 
+            Captain: Luis Garcia, Co-captain: Jason Lin, Members: Jiera Sirivatanarat, Anthony Hallak, Nathan Melwani, Elise Lin
           </p>
         </div>
         <div class="six wide right floated column">
@@ -133,7 +150,7 @@
             Rankings from WRCCDC on <a href="https://twitter.com/wrccdc/status/1358459910946607104">Twitter</a>. <br>
           </p>
           <p>
-            Captain: Anthony Hallak, Linux/Co-captain: Elise Lin, Windows: Jiera Sirivatanarat, Networks: Nathan Melwani, Web: Kobe Braga, Injects: Kanin Liang, Logs: Saul Gonzalez, Floater: Kunal Mittal 
+            Captain: Anthony Hallak, Linux/Co-captain: Elise Lin, Windows: Jiera Sirivatanarat, Networks: Nathan Melwani, Web: Kobe Braga, Injects: Kanin Liang, Logs: Saul Gonzalez, Floater: Kunal Mittal
           </p>
         </div>
         <div class="six wide right floated column">
@@ -154,7 +171,7 @@
             Recognition from UC Riverside on <a href="https://twitter.com/UCRiverside/status/1346860974301564929">Twitter</a>.
           </p>
           <p>
-            Captain: Luis Garcia, Co-captain: Jason Lin, Members: Jiera Sirivatanarat, Anthony Hallak, Nathan Melwani, Elise Lin 
+            Captain: Luis Garcia, Co-captain: Jason Lin, Members: Jiera Sirivatanarat, Anthony Hallak, Nathan Melwani, Elise Lin
           </p>
         </div>
         <div class="six wide right floated column">
@@ -171,7 +188,7 @@
             The come back! Cyber placed 3rd in the defense category. We're seeking to improve our methods by expanding our scripts, getting hands-on with archived machines, and looking for new recruits.
           </p>
           <p>
-            Captain: Edward Huh, Linux/Co-Captain: Anthony Hallak, Windows: Jiera Sirivatanarat, Web: Luis Garcia, Networks: Elise Lin, Injects: Steven Tran, Logs: Renee Pedvin, Floater: Nicholas Pleatsikas 
+            Captain: Edward Huh, Linux/Co-Captain: Anthony Hallak, Windows: Jiera Sirivatanarat, Web: Luis Garcia, Networks: Elise Lin, Injects: Steven Tran, Logs: Renee Pedvin, Floater: Nicholas Pleatsikas
           </p>
         </div>
         <div class="six wide right floated column">
@@ -184,7 +201,7 @@
             2019 Regionals CPTC Team
           </h3>
           <p>
-            Cyber's first offensive security competition @Stanford! We did not place and are hoping to expand our team and skills for 2020. 
+            Cyber's first offensive security competition @Stanford! We did not place and are hoping to expand our team and skills for 2020.
           </p>
           <p>
             Captain: Luis Garcia, Co-captain: Anthony Hallak, Members: Edward Huh, Sean Richardson, Elise Lin
@@ -203,7 +220,7 @@
             Cyber did not qualify but watch for our come back next year!
           </p>
           <p>
-            Captain: Daniel Stinson, Linux: Anthony Hallak, Windows: Brittney Mun, Web: William Shiao, Networks: Edward Huh, Injects: Sophie Schrager, Logs: Raudel Blazquez Munoz, Floater: Luis Garcia 
+            Captain: Daniel Stinson, Linux: Anthony Hallak, Windows: Brittney Mun, Web: William Shiao, Networks: Edward Huh, Injects: Sophie Schrager, Logs: Raudel Blazquez Munoz, Floater: Luis Garcia
           </p>
         </div>
         <div class="six wide right floated column">
@@ -219,7 +236,7 @@
             Cyber placed 3rd overall and 1st in the defense category!
           </p>
           <p>
-            Captain: Bradley Evans, Linux: Calvin Ta, Windows: Brittney Mun, Web: William Shiao, Networks: Daniel Stinson, Injects: Edward Huh, Logs: Renee Pedvin, Floater: Queston Juarez   
+            Captain: Bradley Evans, Linux: Calvin Ta, Windows: Brittney Mun, Web: William Shiao, Networks: Daniel Stinson, Injects: Edward Huh, Logs: Renee Pedvin, Floater: Queston Juarez
           </p>
         </div>
         <div class="six wide right floated column">
@@ -232,7 +249,7 @@
             2017 Regionals CCDC Team
           </h3>
           <p>
-            Cyber placed 5th overall. 
+            Cyber placed 5th overall.
           </p>
           <p>
             Captain: Bradley Evans, Linux: Calvin Ta, Windows: Brittney Mun, Web: William Shiao, Networks: Nicholas Pelham, Injects: Kevin Dinh, Logs: Andrew Nava, Floater: Jerry Jiang
@@ -248,7 +265,7 @@
             2016 Regionals CCDC Team
           </h3>
           <p>
-            Cyber placed 1st in defense! Thank you to UCR's Chief Information Security Officer, Nicholas Turley, and the Computer Science department's Manager of Technical Operations, Victor Hill, for coaching us our starting year. 
+            Cyber placed 1st in defense! Thank you to UCR's Chief Information Security Officer, Nicholas Turley, and the Computer Science department's Manager of Technical Operations, Victor Hill, for coaching us our starting year.
           </p>
           <p>
             Captain: Bradley Evans, Linux: Calvin Ta, Windows: Ho-Ren Kang, Web: Bradd Carey, Networks: Alan Quach , Logs: Marco Tobon, Injects: Montana Esguerra, Floater: Kevin Dinh
@@ -262,7 +279,7 @@
   </sui-segment>
 
 </div>
-  
+
 </template>
 
 <script>
